ae71175dd469ceb8ce42e5899e8207aa1881852d
Selfishx <97510714+selfishx@users.noreply.github.com>
2022-05-26 21:40:47 +0530

    Update start.py

--- GPBot/plugins/start.py
+++ GPBot/plugins/start.py
@@ -3,7 +3,7 @@ from telethon import events, Button
 
 PM_START_TEXT = """
 **Hi {}**
-I am a bot who works for @TgxSupportChat and can detect spammers in groups can protect groups from then
+I am a bot who works for @classy_empire and can detect spammers in groups can protect groups from then
 
 **Click the below button for getting help menu!**
 """
@@ -14,7 +14,7 @@ async def start(event):
     if event.is_private:
        await event.reply(PM_START_TEXT.format(event.sender.first_name), buttons=[
         [Button.inline("Help And Commands", data="help")],
-        [Button.url("Source Code", "GitHub.com/TgxBotz/TelethonGPBot")]])
+        [Button.url("classy empire", "https://t.me/classy_empire")]])
        return
 
     if event.is_group:
